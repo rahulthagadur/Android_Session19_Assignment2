ac13d036891f853893b917fe48e7bb4669bc5e54
rahulthagadur <rt_rahul@hotmail.com>
2017-11-02 21:21:07 +0530

    Initial commit

---
+++ app/src/main/java/com/example/thagadur/android_session19_assignment2/network/OnWebServiceResult.java
@@ -0,0 +1,13 @@
+package com.example.thagadur.android_session19_assignment2.network;
+
+
+
+import com.example.thagadur.android_session19_assignment2.utils.CommonUtilities;
+
+/**
+ * @author Preetika
+ *
+ */
+public interface OnWebServiceResult {
+	public void getWebResponse(String result, CommonUtilities.SERVICE_TYPE type);
+}
